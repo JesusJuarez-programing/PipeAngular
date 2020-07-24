68abd8c86e1357abc999011666f932b58b13e4b0
JesusJuarez-programing <juarez.devora.ja@gmail.com>
2020-07-23 17:07:03 -0700

    Termino

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, resolveForwardRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Iron Man';
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  pi: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 12345.66;
+  nombre2: string = 'AntoNI0';
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 10,
+    direccion: {
+      calle: 'Primavera',
+      casa: 134
+    }
+  }
+
+  valorPromesa = new Promise<string>( (resolve)=> { 
+  setTimeout(() => {
+    resolve('Llego la data');
+  }, 4500);
+});
+
 }
